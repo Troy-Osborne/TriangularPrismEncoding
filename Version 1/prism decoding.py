c595817d676a07c3e3bf1ee93aafd325aef0010c
Troy Osborne <troyosborne515@gmail.com>
2025-04-13 20:39:11 +1000

    Add files via upload
    'correction.py' contains a dictionary lookup to correct any single bit errors.
    'encode.py' contains a dictionary to lookup the corresponding 11 encoded bits for any 6 input bits.
    'valid.py' contains a dictionary of all valid inputs.
    'prism encoding.py' contains the encoder.
    'prism decoding.py' contains the decoder.

---
+++ Version 1/prism decoding.py
@@ -0,0 +1,89 @@
+from correction import correct
+from valid import valid
+from struct import unpack
+
+def decode(ElevenIn):##Assume no errors Exist
+    return ElevenIn[0:6]
+
+def check_and_decode(ElevenIn):##must be tuple
+    if ElevenIn in valid:
+        TrueVal=ElevenIn
+        Trustworthy=True
+    else:
+        print("Value Corrupted")
+        print("Attempting Repair")
+        try:
+            TrueVal=correct(ElevenIn)
+            print("Successful Repair")
+            Trustworthy=True
+        except:
+            print("Can't repair value. Over Corrupted, continuing anyway.\n Repair manually with context clues")
+            TrueVal=correct(ElevenIn)
+            Trustworthy=False
+    return decode(TrueVal),Trustworthy
+
+def bits(B):
+    L=[B&1]
+    B=B>>1
+    for i in range(7):
+        L.append(B&1)
+        B=B>>1
+    return L
+
+def to_bytes(Bits):
+    Out=b""
+    while Bits:
+        N=0
+        if len(Bits)>7:
+            for n in range(8):
+                N+=Bits[7-n]<<n
+            Bits=Bits[8:]
+        else:
+            for n in range(len(Bits)):
+                N+=Bits[n]<<n
+            Bits=Bits[8:]
+        Out+=bytes([N])
+    return Out
+    
+    
+
+class decoder:
+    def __init__(self,InFile):
+        self.InChunk=8*11#every 6x8
+        self.OutChunk=6*8#every 8x11
+        self.InFile=open(InFile,"rb")
+        self.inbuffer=[]
+        self.outbuffer=[]
+    def load_to_memory(self):
+        while 1:
+            Data=self.InFile.read(11)
+            if Data:
+                for i in Data:
+                    currentByte=i
+                    currentBits=bits(currentByte)
+                    self.inbuffer+=currentBits
+                if len(Data)>=2:
+                    self.last2=Data[-2:]
+                elif len(Data)==1:
+                    self.last2=self.last2[-1:]+Data
+            else:
+                break
+        self.extrabits=unpack("BB",self.last2)#Last 2 bytes represent how many bits padding were required to fit into 8 bit chunks
+        self.inbuffer=self.inbuffer[:-(16+self.extrabits[0])]
+        return 1
+    def decode_memory(self):
+        while self.inbuffer:
+            self.outbuffer+=self.inbuffer[:6]
+            self.inbuffer=self.inbuffer[11:]
+        if self.extrabits[1]>0:
+            self.outbuffer=self.outbuffer[:0-self.extrabits[1]]
+
+def Decode_File(filename):
+    D=decoder(filename)
+    D.load_to_memory()
+    D.decode_memory()
+    return to_bytes(D.outbuffer)
+
+if __name__=='__main__':
+    B=Decode_File("prism encoding encoded.enc")
+    print(B.decode())
---
+++ Version 1/prism encoding.py
@@ -0,0 +1,114 @@
+from time import time
+from encode import encode_dict
+from struct import pack
+
+def bits_to_bytes(Bits):
+    Out=b""
+    while Bits:
+        N=0
+        if len(Bits)>7:
+            for n in range(8):
+                N+=Bits[n]<<n
+            Bits=Bits[8:]
+        else:
+            for n in range(len(Bits)):
+                N+=Bits[n]<<n
+            Bits=Bits[8:]
+        Out+=bytes([N])
+    return Out
+    
+def xor_list(L):
+    return sum(L) & 1
+
+def test(reps=10000):
+    Start=time()
+    for i in range(reps):
+        encode((1,0,1,1,0,1))
+    End=time()
+    print(End-Start)
+    return End-Start
+
+def encode_func(SixIn):#Just for human reference
+    #Don't run this function, instead
+    #use the dictionry encode_dict for speed improvements
+    L1,L2,L3,R1,R2,R3=SixIn
+    Faces=[
+        [L1,L2,L3],
+        [R1,R2,R3],
+        [L1,R1,R2,L2],
+        [L2,R2,R3,L3],
+        [L3,R3,R1,L1]]
+    FiveOut=[xor_list(L) for L in Faces]
+    return SixIn+FiveOut
+
+def encode(SixIn):#input must be tuple
+    return encode_dict[SixIn]
+
+class encoder:
+    def __init__(self,OutFile):
+        self.inbuffer=[]
+        self.InChunk=6*8#every 6x8
+        self.OutChunk=8*11#every 8x11
+        self.OutFile=open(OutFile,"wb")
+        self.outbuffer=[]
+    def add_bits(self,In):
+        self.inbuffer+=In
+        self.flush()
+    def flush(self):
+        while len(self.inbuffer)>=self.InChunk:
+            for B in range(8):##remove 8 lots of 6 from in buffer
+                self.outbuffer+=encode(tuple(self.inbuffer[0:6]))##add 8 lots of 11 to the outbuffer
+                self.inbuffer=self.inbuffer[6:]
+        while len(self.outbuffer)>=self.OutChunk:
+            for B in range(11):##remove 11 lots of 8 from out buffer
+                self.OutFile.write(bits_to_bytes(self.outbuffer[0:8]))
+                self.outbuffer=self.outbuffer[8:]
+    def close(self):
+        AddedIn=0
+        AddedOut=0
+        while len(self.inbuffer)%6!=0:
+            self.inbuffer.append(0)
+            AddedIn+=1
+        while self.inbuffer!=[]:
+            self.outbuffer+=encode(tuple(self.inbuffer[0:6]))##add 8 lots of 11 to the outbuffer
+            self.inbuffer=self.inbuffer[6:]
+            
+        while len(self.outbuffer)%8!=0:
+            self.outbuffer.append(0)
+            AddedOut+=1
+        while self.outbuffer!=[]:
+            self.OutFile.write(bits_to_bytes(self.outbuffer[0:8]))
+            self.outbuffer=self.outbuffer[8:]
+        
+        Tail=pack("BB",AddedIn,AddedOut)
+        self.OutFile.write(Tail)
+            
+        self.OutFile.close()
+
+        
+def bin8(i):
+    s=bin(i)[2:]
+    l=len(s)
+    return [0]*(8-l)+list(map(int,s))
+
+def bits(Bytes):
+    out=[]
+    for i in Bytes:
+        out+=bin8(i)
+    return out
+    
+def encode_string(Encoder,string):
+    B=bits(string.encode())
+    Encoder.add_bits(B)
+
+def encode_text_file(InFileName,OutFileName):
+    Encoder=encoder(OutFileName)
+    InF=open(InFileName,'r')
+    Text=InF.read()
+    encode_string(Encoder,Text)
+    InF.close()
+    Encoder.close()
+    return Encoder
+
+if __name__=='__main__':
+    encode_text_file("prism encoding.py","prism encoding encoded.enc")
